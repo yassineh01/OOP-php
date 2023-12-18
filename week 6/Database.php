c29cd9372ea3a7f81d94679f25df1476ff9bc5ba
mustaphaOU21 <133224834+mustaphaOU21@users.noreply.github.com>
2023-12-18 12:12:37 +0100

    Session destroy

---
+++ week 6/Database.php
@@ -0,0 +1,47 @@
+<?php
+
+class Database
+{
+    private $pdo;
+    public $message;
+
+    public function __construct(string $servername = 'localhost', string $username = 'root', string $password = 'QweMus?!123!', string $dbname = 'OOP')
+    {
+        $this->pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function AddUser(string $user_name, string $password, string $email)
+    {
+        $stmt = $this->pdo->prepare("INSERT INTO log (user_name, password, email) VALUES (:user_name, :password, :email)");
+        $stmt->bindParam(':user_name', $user_name);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        return $this->message = 'User added successfully!';
+    }
+
+    public function CheckUser(string $user_name, string $password)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM log WHERE user_name = :user_name");
+        $stmt->bindParam(':user_name', $user_name);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+            session_start();
+            $_SESSION['username'] = $user_name;
+            header('Location: Home.php');
+        } else {
+            $this->message = 'Invalid username or password.';
+            return $this->message;
+        }
+    }
+
+    public function destroy()
+    {
+        session_destroy();
+        header('Location: form.php');
+        exit();
+    }
+}
---
+++ week 6/Home.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'Database.php';
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('Location: form.php');
+    exit();
+} else {
+    $username = $_SESSION['username'];
+}
+$db = new Database();
+if (isset($_POST['logout'])) {
+    $db->destroy();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Hallo</title>
+</head>
+
+<body>
+    <h1 class="text-center">Hallo <?php echo $username ?></h1>
+    <form method="post">
+        <button type="submit" name="logout" class="btn btn-primary">Logout</button>
+    </form>
+</body>
+
+</html>
